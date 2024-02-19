fa10e2924424f97fbef80f32534a839a467e9304
Lara <100379402+runawaytyrant@users.noreply.github.com>
2024-02-18 22:54:16 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const loadWeek1 = document.getElementById('week1');
+const loadWeek2 = document.getElementById('week2');
+const loadWeek3 = document.getElementById('week3');
+const loadAnotherDay = document.getElementById('anotherDay');
+const contentContainer = document.getElementById('principal');
+
+loadWeek1.addEventListener('click', () => {
+    fetch("content/week1.html")
+        .then(response => response.text())
+        .then(html => {
+            contentContainer.innerHTML = html;
+        })
+        .catch(error => console.error("Error fetching content:", error));
+});
+
+loadWeek2.addEventListener('click', () => {
+    fetch("content/week2.html")
+        .then(response => response.text())
+        .then(html => {
+            contentContainer.innerHTML = html;
+        })
+        .catch(error => console.error("Error fetching content:", error));
+});
+
+loadWeek3.addEventListener('click', () => {
+    fetch("content/week3.html")
+        .then(response => response.text())
+        .then(html => {
+            contentContainer.innerHTML = html;
+        })
+        .catch(error => console.error("Error fetching content:", error));
+});
+
+loadAnotherDay.addEventListener('click', () => {
+    fetch("content/anotherDay.html")
+        .then(response => response.text())
+        .then(html => {
+            contentContainer.innerHTML = html;
+        })
+        .catch(error => console.error("Error fetching content:", error));
+});
